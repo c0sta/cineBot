83eaa4264a3243b93fb5f6cfcf5eae45c4193de9
Gabriel Costa <gabriel.moura6@fatec.sp.gov.br>
2019-03-17 16:42:50 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,80 +1,11 @@
-
 const Telegraf = require('telegraf')
-const Extra = require('telegraf/extra')
-const Markup = require('telegraf/markup')
 
-    const bot = new Telegraf(process.env.BOT_TOKEN)
-    bot.token=['627176259:AAE9-8Kit0nzuvoylgW2hU808FaFxmQd0Q8'];
+const bot = new Telegraf('807111944:AAEpwhrE1Pq4v7RrA8BRak6tW-AsjgyjztU', process.env.BOT_TOKEN)
 
-
-    
+bot.start((ctx) => ctx.reply('Olá, sou o GuiaFilmes :) digite /filme e o mês para saber quais filmes serão lançados.'))
 
 
-    // ESCUTADOR DE EVENTOS
-    // bot.hears('💰 Entrada', ctx => ctx.reply('Digite /entrada em seguida o "titulo" e o "valor"'))
-    //bot.hears('💸 Saída', ctx => ctx.reply('Digite /saida em seguida o "titulo" e o "valor"'))
 
-    bot.hears('✔️ Sim', ctx => ctx.reply('Adicionado :)'))
-    bot.hears('❌ Não', ctx => ctx.reply('Não foi adicionado'))
-    
-
-    // COMANDOS
-
-    // ENTRADA E SAIDA DO CHAT
-    bot.command('start', (ctx) => {
-      return ctx.reply('Olá! :) clique em "Adicionar" para inserir um gasto ou um ganho', Extra.markup(
-        Markup.keyboard(['➕ Adicionar'])
-        .resize()
-        .oneTime(true)
-      ))
-    })
-
-    bot.command('quit', (ctx) => {
-      // Using shortcut
-      ctx.leaveChat()
-    })
+bot.launch()
 
 
-    // ADICIONAR  Entrada||Saida
-    const add = (ctx) => {
-      return ctx.reply('Você tem certeza que deseja Adicioná-lo ?', Markup.keyboard([
-        ['✔️ Sim', '❌ Não'],
-      ])
-      .oneTime(true)
-      .resize()
-      .extra()
-     
-        )
-    }
-    
-    const remove = (ctx) => {
-      return ctx.reply('Você tem certeza que deseja remove-lo ?', Markup.keyboard([
-        ['✔️ Sim', '❌ Não'],
-      ])
-      .oneTime(true)
-      .resize()
-      .extra()
-     
-        )
-    }
-
-    bot.hears('➕ Adicionar', (ctx) => {
-    return ctx.reply('Você deseja adicionar uma Entrada ou uma Saída?', Markup.keyboard([
-        ['💰 Entrada', '💸 Saída'], // Row1 with 2 buttons
-        ])
-        .oneTime()
-        .resize()
-        .extra()
-    )})
-
-    bot.hears('💰 Entrada', (ctx) => {
-        return ctx.reply('Digite /entrada em seguida o "titulo" e o "valor"')
-    })
-
-    bot.hears('💸 Saída', (ctx) => {
-        return ctx.reply('Digite /saida em seguida o "titulo" e o "valor"')
-    })
-
-    bot.action(/\/entrada (.+)/ )
-
-    bot.launch()
