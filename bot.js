33d3bb98d3daeae548a80f19fe4647a991ebe8cf
Gabriel Costa <gabriel.moura6@fatec.sp.gov.br>
2019-03-19 19:43:02 -0300

    updated files

--- bot.js
+++ bot.js
@@ -1,10 +1,16 @@
 const Telegraf = require('telegraf')
 const Markup = require('telegraf/markup')
 const Extra = require('telegraf/extra')
+//const imdb = require('imdb-api')*
 const TOKEN = require('./config.js')
 
-const bot = new Telegraf(TOKEN.telegraf_token, process.env.BOT_TOKEN)
+//Comentarios com * são relacionado ao IMDB
 
+// Creating bot *
+const bot = new Telegraf(TOKEN.telegraf_token, process.env.BOT_TOKEN)
+//const cli = new imdb.Client({apiKey: TOKEN.imdb}); *
+
+// On start
 bot.start((ctx) => {
   return ctx.reply('Olá, sou o GuiaFilmes :) \n Clique em 🌍 Local para verificar o Cinemark de sua cidade. \n Clique em 🎬 Programação para verificar os filmes em cartaz do Cinemark de sua região.', Markup
   .keyboard([
@@ -16,7 +22,25 @@ bot.start((ctx) => {
   )
 })
   
-  bot.hears('🔍 Procurar', ctx => ctx.reply('Yay!'))
+  bot.hears('🔍 Procurar', ctx => ctx.reply('Digite "/procurar nomeDoFilme" '))
+
+  
+  bot.hears(/\/procurar (.+)/i, (ctx) => {
+      let resultado;
+      const movie = ctx.match[1];
+      //console.log(movie) 
+      cli.search({'name': movie}).then( (search, ctx) => {
+        for (const result of search.results) {
+          resultado = result;
+        }
+      })
+      
+      
+    })
+
+
+
+
   bot.hears('🌍 Local', ctx => ctx.reply('Local do cinema'))
   bot.hears('🎬 Programação', ctx => ctx.reply('Programação do dia') )
 
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    telegraf_token: '807111944:AAEpwhrE1Pq4v7RrA8BRak6tW-AsjgyjztU',
+    imdb: '2b7dbe9b',
+    code: 'moviebot'
+}
+
